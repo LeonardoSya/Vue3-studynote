4adef71690a0dc23f2c1de874cc324f98cbcc209
LeonardoSya <2246866774@qq.com>
2023-09-02 14:01:47 +0800

    modified:   "01_src_\345\210\206\346\236\220\345\267\245\347\250\213\347\273\223\346\236\204/vite-project/src/App.vue"

--- 01_src_分析工程结构/vite-project/src/App.vue
+++ 01_src_分析工程结构/vite-project/src/App.vue
@@ -1,5 +1,13 @@
+<!--
+ * @Author: LeonardoSya 2246866774@qq.com
+ * @Date: 2023-08-30 14:41:15
+ * @LastEditors: LeonardoSya 2246866774@qq.com
+ * @LastEditTime: 2023-09-02 13:53:59
+ * @FilePath: \Vue\01_src_分析工程结构\vite-project\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
-  <h1>我是App组件</h1>
+  <h1>setup配置项, ref定义响应式数据</h1>
   <h2>{{ name }}</h2>
   <h2>{{ age }}</h2>
   <button @click="sayHello">sayHello</button>
