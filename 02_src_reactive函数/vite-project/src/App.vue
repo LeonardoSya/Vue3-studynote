a5465a8f5cf42f66c6b85b19c5831cba0b2f2225
LeonardoSya <2246866774@qq.com>
2023-10-09 21:50:04 +0800

    modified:   "02_src_reactive\345\207\275\346\225\260/vite-project/src/App.vue"

--- 02_src_reactive函数/vite-project/src/App.vue
+++ 02_src_reactive函数/vite-project/src/App.vue
@@ -2,7 +2,7 @@
  * @Author: LeonardoSya 2246866774@qq.com
  * @Date: 2023-09-02 14:12:40
  * @LastEditors: LeonardoSya 2246866774@qq.com
- * @LastEditTime: 2023-10-09 21:42:08
+ * @LastEditTime: 2023-10-09 21:44:08
  * @FilePath: \Vue\02_src_reactive函数\vite-project\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -15,7 +15,7 @@ export default {
 
   setup() {
 
-    // const 代理对象 = reactive(源对象), 返回一个代理对象(Proxy对象)
+    // const 代理对象 = reactive(源对象), 返回一个代理对象(Proxy的实例对象)
     const person = reactive({
       job: {
         type: '前端',
