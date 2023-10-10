f72f95cb275912c3fb6359aa2bd68a6fdaebefa7
LeonardoSya <2246866774@qq.com>
2023-10-10 09:11:22 +0800

    new file:   "03_src_\345\223\215\345\272\224\345\274\217\345\216\237\347\220\206Proxy\345\257\271\350\261\241/vite-project/.gitignore" 	new file:   "03_src_\345\223\215\345\272\224\345\274\217\345\216\237\347\220\206Proxy\345\257\271\350\261\241/vite-project/README.md" 	new file:   "03_src_\345\223\215\345\272\224\345\274\217\345\216\237\347\220\206Proxy\345\257\271\350\261\241/vite-project/index.html" 	new file:   "03_src_\345\223\215\345\272\224\345\274\217\345\216\237\347\220\206Proxy\345\257\271\350\261\241/vite-project/package.json" 	new file:   "03_src_\345\223\215\345\272\224\345\274\217\345\216\237\347\220\206Proxy\345\257\271\350\261\241/vite-project/pnpm-lock.yaml" 	new file:   "03_src_\345\223\215\345\272\224\345\274\217\345\216\237\347\220\206Proxy\345\257\271\350\261\241/vite-project/public/vite.svg" 	new file:   "03_src_\345\223\215\345\272\224\345\274\217\345\216\237\347\220\206Proxy\345\257\271\350\261\241/vite-project/src/App.vue" 	new file:   "03_src_\345\223\215\345\272\224\345\274\217\345\216\237\347\220\206Proxy\345\257\271\350\261\241/vite-project/src/assets/vue.svg" 	new file:   "03_src_\345\223\215\345\272\224\345\274\217\345\216\237\347\220\206Proxy\345\257\271\350\261\241/vite-project/src/components/HelloWorld.vue" 	new file:   "03_src_\345\223\215\345\272\224\345\274\217\345\216\237\347\220\206Proxy\345\257\271\350\261\241/vite-project/src/main.js" 	new file:   "03_src_\345\223\215\345\272\224\345\274\217\345\216\237\347\220\206Proxy\345\257\271\350\261\241/vite-project/src/style.css" 	new file:   "03_src_\345\223\215\345\272\224\345\274\217\345\216\237\347\220\206Proxy\345\257\271\350\261\241/vite-project/vite.config.js"

---
+++ 03_src_响应式原理Proxy对象/vite-project/src/App.vue
@@ -0,0 +1,42 @@
+<!--
+ * @Author: LeonardoSya 2246866774@qq.com
+ * @Date: 2023-10-10 08:17:09
+ * @LastEditors: LeonardoSya 2246866774@qq.com
+ * @LastEditTime: 2023-10-10 08:24:06
+ * @FilePath: \Vue\03_src_响应式原理Proxy对象\vite-project\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
